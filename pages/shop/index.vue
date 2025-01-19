9bfd39708baae8bd47c79e4facfff2f8e57cf54e
SergeiMarte <konoplyankas@mail.ru>
2025-01-19 14:22:47 +0300

    feat(march-2): Промежуточный комит

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -5,9 +5,75 @@ export default defineComponent({
 </script>
 
 <template>
+    <div class="shop__container container">
+       <div class="shop__content">
+        <div class="shop__category">
 
+        </div>
+        <div class="shop__goods goods">
+            <ul class="goods__meal-list">
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+            </ul>
+            <ul class="goods__alche-list">
+                <li class="goods__alche-item"></li>
+                <li class="goods__alche-item"></li>
+                <li class="goods__alche-item"></li>
+                <li class="goods__alche-item"></li>
+                <li class="goods__alche-item"></li>
+                <li class="goods__alche-item"></li>
+                <li class="goods__alche-item"></li>
+                <li class="goods__alche-item"></li>
+                <li class="goods__alche-item"></li>
+                <li class="goods__alche-item"></li>
+            </ul>
+            <ul class="goods__meal-list">
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+            </ul>
+            <ul class="goods__meal-list">
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+                <li class="goods__meal-item"></li>
+            </ul>
+        </div>
+       </div>
+
+    </div>
 </template>
 
 <style scoped lang="scss">
+.shop__container {
+    // display: flex;
+    padding-top: 40px;
+    padding-bottom: 40px;
+}
 
+.shop__content {
+    background-color: aqua;
+    max-height: 800px;
+}
 </style>
